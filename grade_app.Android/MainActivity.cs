19848fe26ed6381bb25cae6d8a39da1dad508656
Mikhail <mikes22@yandex.ru>
2023-04-18 16:37:59 +0300

    Change Android app icon and name

--- grade_app.Android/MainActivity.cs
+++ grade_app.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace grade_app.Droid
 {
-    [Activity(Label = "grade_app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Сервис БРС", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
