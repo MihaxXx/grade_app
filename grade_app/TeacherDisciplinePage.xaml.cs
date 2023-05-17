8fee2e079eb03780aaa6f411a0868af05dd4a3b2
Mikhail <mikes22@yandex.ru>
2023-05-17 20:22:41 +0300

    Fix display of specName when specAbbr is null at groups names

--- grade_app/TeacherDisciplinePage.xaml.cs
+++ grade_app/TeacherDisciplinePage.xaml.cs
@@ -135,7 +135,7 @@ namespace grade_app
 					foreach (var group in TeacherDiscipline.Students)
 					{
 						var groupInfo = TeacherDiscipline.Groups[group.Key];
-						var disGroup = new DisGroup(groupInfo.Name() + " | " + groupInfo.SpecAbbr ?? groupInfo.SpecName);
+						var disGroup = new DisGroup(groupInfo.Name() + " | " + (groupInfo.SpecAbbr ?? groupInfo.SpecName));
 						foreach (var student in group.Value)
 						{
 							var Rate = TeacherDiscipline.Rates != null && TeacherDiscipline.Rates.ContainsKey(student.RecordBookId) ?
